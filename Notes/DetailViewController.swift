55ee2f517bb1af29169e703c2b7f0aff32eeefcd
Paul Richardson <developer@photographerafoot.com>
2021-05-30 16:54:30 +1000

    CHG: add trailing anchor constraint to text field

--- Notes/DetailViewController.swift
+++ Notes/DetailViewController.swift
@@ -21,13 +21,13 @@ class DetailViewController: UIViewController {
 		if let noteTitle = noteTitle {
 			titleTextField.text = noteTitle
 		}
-		titleTextField.sizeToFit()
 		titleTextField.translatesAutoresizingMaskIntoConstraints = false
 		view.addSubview(titleTextField)
 
 		let constraints = [
 			titleTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
 			titleTextField.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+			titleTextField.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20)
 		]
 
 		NSLayoutConstraint.activate(constraints)
