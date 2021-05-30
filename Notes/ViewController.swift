8994ae160c107c5d5c5081d2953eaad5a78dd747
Paul Richardson <developer@photographerafoot.com>
2021-05-30 14:11:52 +1000

    CHG: navigation bar
    prefer large titles

--- Notes/ViewController.swift
+++ Notes/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UITableViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 
+		navigationController?.navigationBar.prefersLargeTitles = true
 		title = "Notes"
 
 		if notes == nil {
