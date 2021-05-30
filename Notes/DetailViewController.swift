87c45d88ea3c40958842ea2851540a3badfd38ba
Paul Richardson <developer@photographerafoot.com>
2021-05-30 10:44:32 +1000

    CHG: Clean up

--- Notes/DetailViewController.swift
+++ Notes/DetailViewController.swift
@@ -26,19 +26,8 @@ class DetailViewController: UIViewController {
 			titleTextField.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
 		]
 
-				NSLayoutConstraint.activate(constraints)
+		NSLayoutConstraint.activate(constraints)
 
 	}
-
-
-	/*
-	// MARK: - Navigation
-
-	// In a storyboard-based application, you will often want to do a little preparation before navigation
-	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-	// Get the new view controller using segue.destination.
-	// Pass the selected object to the new view controller.
-	}
-	*/
 
 }
