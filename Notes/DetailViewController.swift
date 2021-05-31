c1e284b216b7a50b91e4e8e01c43555910e2ac23
Paul Richardson <developer@photographerafoot.com>
2021-05-31 12:37:11 +1000

    CHG: Adjust for keyboard in text view

--- Notes/DetailViewController.swift
+++ Notes/DetailViewController.swift
@@ -36,10 +36,32 @@ class DetailViewController: UIViewController {
 
 		NSLayoutConstraint.activate(constraints)
 
+		let notificationCenter = NotificationCenter.default
+		notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
+		notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+
 	}
 
 	@objc func save() {
 		//
 	}
 
+	@objc func adjustForKeyboard(notification: Notification) {
+		guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+
+		let keyboardScreenEndFrame = keyboardValue.cgRectValue
+		let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
+
+		if notification.name == UIResponder.keyboardWillHideNotification {
+			noteTextView.contentInset = .zero
+		} else {
+			noteTextView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height - view.safeAreaInsets.bottom, right: 0)
+		}
+
+		noteTextView.scrollIndicatorInsets = noteTextView.contentInset
+
+		let selectedRange = noteTextView.selectedRange
+		noteTextView.scrollRangeToVisible(selectedRange)
+
+	}
 }
